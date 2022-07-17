e4d60cd179bc9a1a64ef2d7d5feb380bf64c5384
Jonathan Ruge <53587500+ruge2109-Git@users.noreply.github.com>
2022-07-17 17:12:36 -0500

    feature: Clean main

--- src/main/java/com/modyo/pokeapi/PokeapiApplication.java
+++ src/main/java/com/modyo/pokeapi/PokeapiApplication.java
@@ -1,37 +1,15 @@
 package com.modyo.pokeapi;
 
-import java.time.Duration;
-import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableCaching
-//@EnableWebSecurity
 public class PokeapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PokeapiApplication.class, args);
     }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration cc = new CorsConfiguration();
-//        cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization"));
-//        cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-//        cc.setAllowedOrigins(Arrays.asList("/*"));
-//        cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "PATCH"));
-//        cc.addAllowedOrigin("*");
-//        cc.setMaxAge(Duration.ZERO);
-//        cc.setAllowCredentials(Boolean.TRUE);
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", cc);
-//        return source;
-//    }
 }
